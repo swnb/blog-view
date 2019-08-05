c42e2a2f33604ebd09c3682ddd7ee5f04cd640ea
swnb <swnblovego@gmail.com>
2019-08-05 14:04:26 +0800

    feat(header) : add bottom for header

--- src/header/index.tsx
+++ src/header/index.tsx
@@ -2,20 +2,31 @@ import React from 'react';
 import { CommandBar } from './nav-bar';
 import { SearchBox } from './search';
 
-const style = {
-	width: '77%',
-	height: '50px',
-	margin: 'auto',
-	display: 'flex',
-	alignItems: 'center',
-	justifyContent: 'space-between'
+const styles = {
+	header: {
+		width: '77%',
+		height: '50px',
+		margin: 'auto',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'space-between'
+	},
+	hr: {
+		margin: '0'
+	}
 };
 
-const Header = () => (
-	<header style={style}>
-		<CommandBar />
-		<SearchBox />
-	</header>
-);
+const Header = () => {
+	const { header, hr } = styles;
+	return (
+		<>
+			<header style={header}>
+				<CommandBar />
+				<SearchBox />
+			</header>
+			<hr style={hr} />
+		</>
+	);
+};
 
 export default Header;
