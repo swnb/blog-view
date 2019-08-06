843ee276e5b70a0c70d59c13484e5d5c25f4b767
swnb <swnblovego@gmail.com>
2019-08-07 01:52:56 +0800

    feat (app) : assembling basic accessories

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import 'icon';
+import Header from 'header';
+import Router from 'router';
 import './App.css';
 
 const App: React.FC = () => {
-	return <> {'init project'}</>;
+	return (
+		<>
+			<Header />
+			<Router />
+		</>
+	);
 };
 
 export default App;
