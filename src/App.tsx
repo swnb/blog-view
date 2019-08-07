7e41325e8590fdddf38c1c5a405f71b26f388531
swnb <swnblovego@gmail.com>
2019-08-07 16:25:43 +0800

    feat (breadcrumb) : set navigationbar down

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import 'icon';
 import Header from 'header';
 import Router from 'router';
+import NavigationBar from 'breadcrumb';
 
 const App: React.FC = () => {
 	return (
 		<>
 			<Header />
+			<NavigationBar />
 			<Router />
 		</>
 	);
