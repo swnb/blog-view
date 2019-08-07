b017efbbe453b0065f1ee1fc95fc1346ec6a1c75
swnb <swnblovego@gmail.com>
2019-08-07 16:35:23 +0800

    feat : init about and archive page

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,8 @@ import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { Home } from 'home';
 import { Paper } from './paper';
 import { NotFound } from './not-found';
+import About from 'about';
+import Archive from 'archive';
 
 const Router: React.FC = () => {
 	return (
@@ -11,6 +13,8 @@ const Router: React.FC = () => {
 				<Redirect exact from="/" to="/home" />
 				<Route path="/home" component={Home} />
 				<Route path="/paper/:id" component={Paper} />
+				<Route path="/about" component={About} />
+				<Route path="/archive" component={Archive} />
 				<Route component={NotFound} />
 			</Switch>
 		</HashRouter>
